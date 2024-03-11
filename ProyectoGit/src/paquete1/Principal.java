94afc3ad8a98dd36340c4d123c49dc0fdddc7720
alvaro <alvaro@172.16.20.116>
2024-03-11 20:43:00 +0100

    version inicial del programa 0.1

---
+++ ProyectoGit/src/paquete1/Principal.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("provando repositorios git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
