e370316f21c24192160d02f1fab45e04da7e588e
alvaro <alvaro@172.16.20.116>
2024-03-12 16:13:33 +0100

    version preliminar rama1

---
+++ ProyectoGit/src/paqueteRama1/Rama1.java
@@ -0,0 +1,11 @@
+package paqueteRama1;
+
+public class Rama1 {
+
+	public static void main(String[] args) {
+		System.out.println("esta es la rama 1");
+		// TODO Auto-generated method stub
+
+	}
+
+}
