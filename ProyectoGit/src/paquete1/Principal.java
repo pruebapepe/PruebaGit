aa74a91fe94219a0f23e19ab27fe415816dec649
alvaro <alvaro@172.16.20.116>
2024-03-12 15:37:29 +0100

    otra version mas

--- ProyectoGit/src/paquete1/Principal.java
+++ ProyectoGit/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("probando repositorios git, parte de prueba");
 		System.out.println("tercera parte de esta aventura");
+		System.out.println("viva el betis");
 		// TODO Auto-generated method stub
 
 	}
